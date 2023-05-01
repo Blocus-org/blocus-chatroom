7347a1adb26e8ccbf0fd10544d3773452131b434
Zqfd <zqfdrshj@protonmail.com>
2023-05-01 17:36:36 +0200

    Removed image requirement
    Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

--- login.php
+++ login.php
@@ -3,9 +3,8 @@
   if(isset($_SESSION['unique_id'])){
     header("location: users.php");
   }
+  include_once "header.php"; 
 ?>
-
-<?php include_once "header.php"; ?>
 <body>
   <div class="wrapper">
     <section class="form login">
@@ -28,9 +27,7 @@
       <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
     </section>
   </div>
-  
   <script src="javascript/pass-show-hide.js"></script>
   <script src="javascript/login.js"></script>
-
 </body>
 </html>
