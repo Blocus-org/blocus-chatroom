051dfcf136ee8a6944c56ca4770a99777f0d3511
Zqfd <zqfdrshj@protonmail.com>
2023-05-15 16:06:31 +0200

    Added more config
    Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

--- php/config.php
+++ php/config.php
@@ -10,8 +10,15 @@
 // CONFIG
 
 // Admin config:
-$admin_interface = true; // If "true", you will be asked to create an admin account in update.php script.
+$admin_interface = 1; // If "1", you will be asked to create an admin account in update.php script.
 $admin_mail = "blocus-org@proton.me"; // Email adress for privacy policy contact, change it to match yours.
+$donation_links =
+array(
+  "links"=>
+      "https://patreon.com/Blocus",
+  //  "https://an.exemple.com", "https://another-one.com", "https://another-one.com"
+  //   Add your link(s) here, please do not remove mines :D --WIP
+);
 
 // Database config:
 $hostname = "localhost";
@@ -21,15 +28,31 @@ $dbname = "blocus_chat_db";
 
 // Footer config:
 $app_name = "Blocus-chatroom";
-$app_version = "beta-2.0.4-nightly";
+$app_version = "beta-2.0.5<br>nightly";
 $source_url = ""; // If you run a modified version, create a public repository (LICENCE: GNU-AGPL-v3.0-or-later).
 
-// Encryption key:
-$encryption_key = ")J@NcRfUjXn2r5u8x/A%D*G-KaPdSgVk"; // !!! Change it to another auto-generated SHA256 key !!!
+// Encryption config:
+$algorithm = "AES-256-CTR";
+$encryption_key = ")J@NcRfUjXn2r5u8x/A%D*G-KaPdSgVk"; // !!! Change it to another auto-generated AES-256 key !!!
 
 // END
 
 // FUNCTIONS
+
+// Set donate link(s)
+if (isset($donation_links)) {
+  if($donation_links != ""){
+    if(count($donation_links) === 1){
+      $donate = '<p><a href="'.$donation_links['links'].'">Donate ❤</a></p>';
+    }else{
+      $donate = '<p><a href="../donate">Donate ❤</a></p>';
+    }
+  }else{
+    $donate = '<p><a href="'.$donation_links['links'].'">Donate ❤</a></p>';
+  }
+}else{
+  $donate = '<p><a href="https://patreon.com/Blocus">Donate ❤</a></p>';
+}
 
 // Set link to source code.
 if($source_url != ""){
@@ -53,6 +76,10 @@ function sec($data){
 }
 
 // END
+
+// Test zone for update.php DO NOT MODIFY
+
+$balise = true;
 
 ?>
 
