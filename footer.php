f0af7f93cead606529c99e29a86764662139d0c5
Zqfd <zqfdrshj@protonmail.com>
2023-05-15 16:04:57 +0200

    Added 'donate' button
    Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <p class="version_footer"><?= $app_name?> <?= $app_version?></p><p class="source_footer">GNU AGPL-v3.0 <a href=<?= $source_code?>>Source</a></p>
+    <p><?= $app_name?> <?= $app_version?></p> <?= $donate?> <p>GNU AGPL-v3.0<br><a href=<?= $source_code?>>Source</a></p>
 </div>
