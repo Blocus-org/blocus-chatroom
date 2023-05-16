c09943ff55653a798f386f75f41bb8bf6954b93f
Zqfd <zqfdrshj@protonmail.com>
2023-05-16 08:42:54 +0200

    init
    Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

---
+++ donate.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'php/config.php';
+if(isset($donation_links) && !empty($donation_links)){
+	if(count($donation_links) === 1){
+		header("location: https://paypal.me/orgBlocus");
+	}else{
+		var_dump($donation_links);
+	}
+}
+?>
