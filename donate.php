b407df586563baa6af8dc4542623303120ce71f1
Zqfd <zqfdrshj@protonmail.com>
2026-03-10 14:56:35 +0100

    Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

--- donate.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'php/config.php';
-if (isset($donation_links) && !empty($donation_links)) {
-	if (count($donation_links) === 1) {
-		header("location: https://paypal.me/orgBlocus");
-	}
-}
--- footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer">
-    <p><?= $app_name?> <?= $app_version?></p> <?= $donate?> <p>GNU AGPL-v3.0<br><a href=<?= $source_code?>>Source</a></p>
-</div>
